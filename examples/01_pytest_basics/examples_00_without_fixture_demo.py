7e6d07f2194e0b06fca8560339af86e3e9b6d6bb
pyneng <pyneng.course@gmail.com>
2023-01-08 12:18:13 +0000

    UPD 01

--- examples/01_pytest_basics/examples_00_without_fixture_demo.py
+++ examples/01_pytest_basics/examples_00_without_fixture_demo.py
@@ -1,24 +1,31 @@
+topology = {
+    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+}
+normalized_topology = {
+    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+}
 
 
 def delete_dupl(topology_dict):
-    pass
+    """dummy function"""
+    return normalized_topology
 
 
 class Topology:
-    def __init__(self, topology):
-        pass
+    """dummy class"""
 
-topology = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-            ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
-            ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
-            ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
-            ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
-            ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
-            ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
-normalized_topology = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-                       ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
-                       ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
-                       ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+    def __init__(self, topology):
+        self.topology = normalized_topology
+
 
 def test_delete_dupl():
     assert delete_dupl(topology) == normalized_topology
@@ -27,4 +34,3 @@ def test_delete_dupl():
 def test_topology_class():
     top = Topology(topology)
     assert top.topology == normalized_topology
-
--- examples/01_pytest_basics/examples_01_fixture_demo.py
+++ examples/01_pytest_basics/examples_01_fixture_demo.py
@@ -2,37 +2,54 @@ import pytest
 
 
 def delete_dupl(topology_dict):
-    pass
+    return {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+    }
 
 
 class Topology:
     def __init__(self, topology):
-        pass
-
+        self.topology = {
+            ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+            ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+            ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+            ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+            ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+            ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        }
 
 
 @pytest.fixture()
 def topology_with_dupl_links():
-    topology = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-                ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
-                ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
-                ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
-                ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
-                ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
-                ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
-                ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
-                ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
+    topology = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+    }
     return topology
 
 
 @pytest.fixture()
 def normalized_topology_example():
-    normalized_topology = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-                           ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
-                           ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
-                           ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
-                           ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
-                           ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
+    normalized_topology = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+    }
     return normalized_topology
 
 
@@ -43,4 +60,3 @@ def test_delete_dupl(topology_with_dupl_links, normalized_topology_example):
 def test_topology_class(topology_with_dupl_links, normalized_topology_example):
     top = Topology(topology_with_dupl_links)
     assert top.topology == normalized_topology_example
-
