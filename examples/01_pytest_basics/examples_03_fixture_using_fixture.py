e7e48c4e0511edfa195212fb887468d3820efb20
pyneng <pyneng.course@gmail.com>
2023-01-08 08:13:49 +0000

    UPD 01

--- examples/01_pytest_basics/examples_03_fixture_using_fixture.py
+++ examples/01_pytest_basics/examples_03_fixture_using_fixture.py
@@ -1,3 +1,4 @@
+# pytest examples_03_fixture_using_fixture.py -v --setup-show --no-header
 import pytest
 from netmiko import Netmiko
 
@@ -9,9 +10,8 @@ def send_show_command(device, command):
     return result
 
 
-@pytest.fixture()
-def r1_test_connection():
-    # setup
+@pytest.fixture(scope="session")
+def r1_params():
     params = dict(
         host="192.168.100.1",
         username="cisco",
@@ -19,24 +19,42 @@ def r1_test_connection():
         secret="cisco",
         device_type="cisco_ios",
     )
-    r1 = Netmiko(**params)
+    return params
+
+
+@pytest.fixture(scope="session")
+def r1_test_connection(r1_params):
+    # setup
+    r1 = Netmiko(**r1_params)
     r1.enable()
     yield r1
     # teardown
     r1.disconnect()
 
 
-def test_send_show_command(r1_test_connection):
-    params = dict(
-        host="192.168.100.1",
-        username="cisco",
-        password="cisco",
-        secret="cisco",
-        device_type="cisco_ios",
-    )
+def test_send_show(r1_test_connection, r1_params):
     cmd = "sh run | i hostname"
-    output = send_show_command(params, cmd)
+    output = send_show_command(r1_params, cmd)
+    correct_output = r1_test_connection.send_command(cmd)
+    # r1_params["password"] = "1231398714895"
+    assert output == correct_output
+
+
+def test_send_show_command(r1_test_connection, r1_params):
+    cmd = "sh run | i hostname"
+    output = send_show_command(r1_params, cmd)
     correct_output = r1_test_connection.send_command(cmd)
     assert output == correct_output
 
-# pytest examples_03_fixture_using_fixture.py -v --setup-show --no-header
+
+def test_send_show_command_intf(r1_test_connection, r1_params):
+    cmd = "sh run | i interface"
+    output = send_show_command(r1_params, cmd)
+    correct_output = r1_test_connection.send_command(cmd)
+    assert output == correct_output
+
+
+def test_send_show_command_sh_intf(r1_params):
+    cmd = "sh ip int br"
+    output = send_show_command(r1_params, cmd)
+    assert r1_params["host"] in output
