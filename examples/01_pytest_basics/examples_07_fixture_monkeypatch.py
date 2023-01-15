e52c1b9a921e6308454f8369a3be472ae88bb7e5
pyneng <pyneng.course@gmail.com>
2023-01-15 16:01:38 +0000

    UPD 01_pytest_basics

--- examples/01_pytest_basics/examples_07_fixture_monkeypatch.py
+++ examples/01_pytest_basics/examples_07_fixture_monkeypatch.py
@@ -1,9 +1,11 @@
+import getpass
 
-def check_passwd(username, password, min_length=8, check_username=True):
+
+def check_passwd(username, password, min_length=8):
     if len(password) < min_length:
         print("Пароль слишком короткий")
         return False
-    elif check_username and username in password:
+    elif username.lower() in password.lower():
         print("Пароль содержит имя пользователя")
         return False
     else:
@@ -13,7 +15,13 @@ def check_passwd(username, password, min_length=8, check_username=True):
 
 def prompt_password(username, password=None, **kwargs):
     if password is None:
-        password = input("Введите пароль: ")
+        # password = input("Введите пароль: ")
+        password = getpass.getpass()
 
     return check_passwd(username, password, **kwargs)
 
+
+def test_prompt_password_input(monkeypatch):
+    # monkeypatch.setattr('builtins.input', lambda prompt=None: '123456789')
+    monkeypatch.setattr(getpass, "getpass", lambda x=None: '123456789')
+    assert prompt_password("user1", None) == True
