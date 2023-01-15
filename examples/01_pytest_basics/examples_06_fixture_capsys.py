016a5b8f1196a173ec81df5d2edd12390351b3bc
pyneng <pyneng.course@gmail.com>
2023-01-15 15:50:48 +0000

    UPD 01_pytest_basics

--- examples/01_pytest_basics/examples_06_fixture_capsys.py
+++ examples/01_pytest_basics/examples_06_fixture_capsys.py
@@ -1,10 +1,11 @@
+import pytest
 
 
-def check_passwd(username, password, min_length=8, check_username=True):
+def check_passwd(username, password, min_length=8):
     if len(password) < min_length:
         print("Пароль слишком короткий")
         return False
-    elif check_username and username in password:
+    elif username.lower() in password.lower():
         print("Пароль содержит имя пользователя")
         return False
     else:
@@ -12,3 +13,13 @@ def check_passwd(username, password, min_length=8, check_username=True):
         return True
 
 
+@pytest.mark.parametrize("min_len, result", [(4, True), (8, False), (6, True)])
+def test_check_passwd_min_length(min_len, result, capsys):
+    user = "user1"
+    password = "123456"
+    assert check_passwd(user, password, min_length=min_len) == result
+    out = capsys.readouterr().out
+    if result == False:
+        assert "короткий" in out
+    else:
+        assert "прошел все проверки" in out
