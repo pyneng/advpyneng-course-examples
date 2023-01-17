f2b2a480df5f148239169d0b8bc8bb8e550af69f
pyneng <pyneng.course@gmail.com>
2023-01-17 15:41:39 +0000

    UPD 01_pytest_basics

--- examples/01_pytest_basics/examples_07_fixture_monkeypatch.py
+++ examples/01_pytest_basics/examples_07_fixture_monkeypatch.py
@@ -1,4 +1,4 @@
-import getpass
+import pytest
 
 
 def check_passwd(username, password, min_length=8):
@@ -13,16 +13,48 @@ def check_passwd(username, password, min_length=8):
         return True
 
 
-def prompt_password(username, password=None, **kwargs):
+def prompt_passwd(username, password=None, **kwargs):
     if password is None:
-        # password = input("Введите пароль: ")
-        password = getpass.getpass()
-
+        password = input("Введите пароль: ")
     return check_passwd(username, password, **kwargs)
 
 
-def test_prompt_password_input(monkeypatch):
-    # monkeypatch.setattr('builtins.input', lambda prompt=None: '123456789')
-    monkeypatch.setattr("getpass.getpass", lambda x=None: '123456789')
-    # monkeypatch.setattr(getpass, "getpass", lambda x=None: '123456789')
-    assert prompt_password("user1", None) == True
+def test_prompt_passwd(monkeypatch):
+    def myinput(prompt=None):
+        return "12346128742"
+
+    monkeypatch.setattr("builtins.input", myinput)
+    # monkeypatch.setattr("builtins.input", lambda prompt=None: "203987928734")
+    assert prompt_passwd("user1") == True
+
+
+@pytest.mark.parametrize(
+    "user, passwd, min_len, correct_result",
+    [
+        ("user1", "123user1w456", 8, False),
+        ("user1", "123453245346", 6, True),
+    ],
+)
+def test_check_passwd_param(
+    user, passwd, min_len, correct_result, monkeypatch
+):
+    monkeypatch.setattr("builtins.input", lambda prompt=None: passwd)
+    assert prompt_passwd(user, min_length=min_len) == correct_result
+
+
+@pytest.mark.parametrize(
+    "user, passwd, min_len, correct_result",
+    [
+        ("user1", "123user1w456", 8, False),
+        ("user1", "123453245346", 6, True),
+    ],
+)
+def test_check_passwd_param_2(
+    user, passwd, min_len, correct_result, monkeypatch
+):
+    def myinput(prompt=None):
+        return passwd
+
+    monkeypatch.setattr("builtins.input", myinput)
+
+    assert prompt_passwd(user, min_length=min_len) == correct_result
