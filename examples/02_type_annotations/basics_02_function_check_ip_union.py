a8008c71ed5f75c44dd6b9b23258cd7796145130
pyneng <pyneng.course@gmail.com>
2023-01-23 15:58:51 +0000

    UPD type annotations

--- examples/02_type_annotations/basics_02_function_check_ip_union.py
+++ examples/02_type_annotations/basics_02_function_check_ip_union.py
@@ -2,13 +2,13 @@ import ipaddress
 
 
 def check_ip(ip: str | int) -> bool:
-    if type(ip) == str and ip in range(16777216, 4026531840):
+    if type(ip) == int and 16777216 < ip < 3758096383:
         return True
     elif type(ip) == str:
         try:
             ipaddress.ip_address(ip)
             return True
-        except ValueError as err:
+        except ValueError:
             return False
     else:
         raise ValueError
@@ -17,5 +17,5 @@ def check_ip(ip: str | int) -> bool:
 if __name__ == "__main__":
     print(check_ip("10.1.1.1"))
     print(check_ip("test"))
-    print(check_ip(100))
-
+    print(check_ip(16777300))
+    # print(check_ip(10.5))
