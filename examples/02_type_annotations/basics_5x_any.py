823bc005ab81c72236dec9f20a027efc1fedb887
pyneng <pyneng.course@gmail.com>
2023-01-24 15:02:54 +0000

    UPD type annotations

--- examples/02_type_annotations/basics_5x_any.py
+++
@@ -1,6 +0,0 @@
-
-class MyNetmiko(CiscoIosBase):
-    def send_command(self, command: str, *args: Any, **kwargs: Any):
-        print(command)
-        super().send_command(command, *args, **kwargs)
-
