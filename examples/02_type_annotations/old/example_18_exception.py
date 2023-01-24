cfb28a2f4409966eeca2a63e767c5e7146e731e3
pyneng <pyneng.course@gmail.com>
2023-01-24 14:55:01 +0000

    UPD type annotations

--- examples/02_type_annotations/old/example_18_exception.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Union, List, Any
-
-
-def summ(x: int, y: int) -> int:
-    if isinstance(x, int) and isinstance(y, int):
-        return x + y
-    else:
-        raise ValueError('Аргументы должны быть числами')
