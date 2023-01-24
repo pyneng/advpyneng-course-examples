c30390819888aa3640426b2ea04d82b678e6d2d8
pyneng <pyneng.course@gmail.com>
2023-01-24 14:46:25 +0000

    UPD type annotations

--- examples/02_type_annotations/basics_14_ignore_module.py
+++ examples/02_type_annotations/basics_14_ignore_module.py
@@ -1,7 +1,6 @@
 from pprint import pprint
 import ipaddress
-
-from pyneng_cli_course.utils import green, red # type: ignore
+from advpyneng_cli_course.utils import green, red # type: ignore
 
 
 def check_ip(ip: str) -> bool:
