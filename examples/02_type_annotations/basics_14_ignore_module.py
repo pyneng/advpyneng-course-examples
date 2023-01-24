9a06ec07a17aa256812311720d129fa1a68284df
pyneng <pyneng.course@gmail.com>
2023-01-24 13:45:00 +0000

    UPD type annotations

---
+++ examples/02_type_annotations/basics_14_ignore_module.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+import ipaddress
+
+from pyneng_cli_course.utils import green, red # type: ignore
+
+
+def check_ip(ip: str) -> bool:
+    try:
+        ipaddress.ip_address(ip)
+        print(green(ip))
+        return True
+    except ValueError as err:
+        print(red(ip))
+        return False
+
+
+if __name__ == "__main__":
+    print(check_ip("10.1.1.1"))
+    print(check_ip("test"))
