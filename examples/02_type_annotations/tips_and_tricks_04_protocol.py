4a9fbf76347c343b60050c24275025884987be6d
pyneng <pyneng.course@gmail.com>
2023-01-25 08:39:38 +0000

    UPD type annotations

---
+++ examples/02_type_annotations/tips_and_tricks_04_protocol.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+import time
+from typing import Protocol
+
+
+class Readable(Protocol):
+    def read(self, amount: int) -> str: ...
+
+
+class FileStream:
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.stream = open(self.filename)
+
+    def read(self, amount: int) -> str:
+        return self.stream.read(amount)
+
+
+def read_stream(stream: Readable, part_size: int) -> None:
+    while True:
+        part = stream.read(part_size)
+        if part:
+            pprint(part)
+            time.sleep(0.2)
+        else:
+            break
+
+
+if __name__ == "__main__":
+    f = FileStream("output/sh_cdp_n_det.txt")
+    read_stream(f, 100)
