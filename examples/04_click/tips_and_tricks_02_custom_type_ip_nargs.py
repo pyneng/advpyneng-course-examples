86342fdc382401c451cd82f38a1763e520897c02
pyneng <pyneng.course@gmail.com>
2023-01-31 20:03:32 +0000

    UPD 04_click

---
+++ examples/04_click/tips_and_tricks_02_custom_type_ip_nargs.py
@@ -0,0 +1,32 @@
+import subprocess
+from pprint import pprint
+import click
+
+
+def ping_ip(ip_address, count):
+    """
+    Ping IP_ADDRESS and return True/False
+    """
+    reply = subprocess.run(
+        f"ping -c {count} -n {ip_address} -W 1",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if reply.returncode == 0:
+        return True
+    else:
+        return False
+
+@click.command()
+@click.argument("ipv4")
+@click.option("-c", "--count", type=int)
+# @click.option("-c", "--count", default=2, show_default=True)
+def cli(ipv4, count):
+    print(f"{ipv4=} {count=}")
+    status = ping_ip(ipv4, count)
+    print(f"RESULTS {ipv4=} {status=}")
+
+if __name__ == "__main__":
+    cli()
