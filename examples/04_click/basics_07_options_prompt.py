d69338e489c5e9260c04a1a463ec742e448f4b12
pyneng <pyneng.course@gmail.com>
2023-02-01 07:59:00 +0000

    UPD 04_click

---
+++ examples/04_click/basics_07_options_prompt.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+import click
+
+
+def send_command_to_devices(devices, command, limit=10):
+    return f"OUTPUT: {command}\n{devices}"
+
+
+@click.command()
+@click.argument("command")
+@click.argument("hosts", nargs=-1, required=True)
+@click.option("-u", "--username", prompt=True)
+@click.option("-p", "--password", prompt=True, hide_input=True)
+@click.option("-s", "--secret", prompt=True, hide_input=True)
+def cli(command, hosts, username, password, secret):
+    device_params = {
+        "device_type": "cisco_ios",
+        "username": username,
+        "password": password,
+        "secret": secret,
+    }
+
+    device_list = [{**device_params, "host": ip} for ip in hosts]
+
+    result = send_command_to_devices(device_list, command)
+    for ip, output in zip(hosts, result):
+        print(ip.center(30, "="))
+        print(output)
+
+
+if __name__ == "__main__":
+    cli()
