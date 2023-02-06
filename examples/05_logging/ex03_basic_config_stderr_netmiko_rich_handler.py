987a9a44fc5a61cdde56b7d9ad6b30f4f0c2ec4c
pyneng <pyneng.course@gmail.com>
2023-02-06 12:31:00 +0000

    UPD 05_logging

--- examples/05_logging/ex03_basic_config_stderr_netmiko_rich_handler.py
+++ examples/05_logging/ex03_basic_config_stderr_netmiko_rich_handler.py
@@ -7,19 +7,25 @@ import logging
 
 import yaml
 from netmiko import ConnectHandler, NetMikoAuthenticationException
-from netmiko.ssh_exception import SSHException
+from netmiko.exceptions import SSHException
 from rich.logging import RichHandler
 
 
 logging.getLogger("paramiko").setLevel(logging.WARNING)
 logging.getLogger("netmiko").setLevel(logging.INFO)
 
+fmt = logging.Formatter("{asctime} {name} {levelname} {message}", style="{")
+lfile = logging.FileHandler("logfile.txt")
+lfile.setLevel(logging.DEBUG)
+lfile.setFormatter(fmt)
+
 logging.basicConfig(
     level=logging.DEBUG,
     format="{message}",
     style="{",
     datefmt="%X",
-    handlers=[RichHandler()],
+    handlers=[RichHandler(), lfile],
+    # handlers=[RichHandler(rich_tracebacks=True)],
     # handlers=[RichHandler(show_path=False)], # отключить вывод имени файла и строки
 )
 
@@ -36,11 +42,11 @@ def send_show(device_dict, command):
             logging.debug(f"Получен вывод команды {command} с {ip}")
         return result
     except SSHException as error:
-        # logging.exception(f"Ошибка {error} на {ip}") # вывод с traceback
-        logging.error(f"Ошибка {error} на {ip}")
+        # logging.exception(f"Ошибка {type(error)} на {ip}") # вывод с traceback
+        logging.error(f"Ошибка {type(error)} на {ip}")
 
 
-def send_command_to_devices(devices, command, threads=10):
+def send_command_to_devices(devices, command, threads=2):
     data = {}
     with ThreadPoolExecutor(max_workers=threads) as executor:
         result = executor.map(send_show, devices, repeat(command))
@@ -52,4 +58,4 @@ def send_command_to_devices(devices, command, threads=10):
 if __name__ == "__main__":
     with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-    pprint(send_command_to_devices(devices, "sh clock"))
+    send_command_to_devices(devices, "sh clock")
