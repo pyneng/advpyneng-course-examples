d64640058fc11eeee9972129359afaa945e3887d
pyneng <pyneng.course@gmail.com>
2023-02-08 09:20:54 +0000

    UPD logging

--- examples/05_logging/basics_04_logging_api_stderr.py
+++ examples/05_logging/basics_04_logging_api_stderr.py
@@ -2,15 +2,20 @@ import sys
 import logging
 from rich import inspect
 
+# logger
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
-fmt = logging.Formatter("{asctime} {name} {levelname} {message}", style="{")
+fmt = logging.Formatter(
+    "{asctime} {name} {levelname} {message}",
+    style="{",
+    datefmt="%H:%M:%S",
+)
+
 stderr = logging.StreamHandler(sys.stdout)
 stderr.setLevel(logging.INFO)
 stderr.setFormatter(fmt)
 
-# logger
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
 log.addHandler(stderr)
 
 ## messages
