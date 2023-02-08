3f6a29bd36fa028e18562bcaaf1b4f9aaa65365f
pyneng <pyneng.course@gmail.com>
2023-02-08 18:34:07 +0000

    UPD logging

--- examples/05_logging/basics_10_basic_config_handlers_stderr_file.py
+++ examples/05_logging/basics_10_basic_config_handlers_stderr_file.py
@@ -1,17 +1,13 @@
 import logging
 
-
-# file
-logfile = logging.FileHandler("log10.txt")
-logfile.setLevel(logging.INFO)
-
-# stderr
 stderr = logging.StreamHandler()
 stderr.setLevel(logging.DEBUG)
 
+logfile = logging.FileHandler("log10.txt")
+logfile.setLevel(logging.DEBUG)
 
 logging.basicConfig(
-    handlers=[logfile, stderr],
+    handlers=[stderr, logfile],
     level=logging.DEBUG,
     format="{asctime} {levelname:10} {name} {message}",
     style="{",
