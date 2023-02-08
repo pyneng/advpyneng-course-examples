c15caecad455756c8009a4f798433951df518233
pyneng <pyneng.course@gmail.com>
2023-02-08 18:52:01 +0000

    UPD logging

--- examples/05_logging/tips_and_tricks_04_logging_api_rich_handler.py
+++ examples/05_logging/tips_and_tricks_04_logging_api_rich_handler.py
@@ -4,23 +4,35 @@ from netmiko import Netmiko
 from rich.logging import RichHandler
 
 
-logging.getLogger("netmiko").setLevel(logging.INFO)
-logging.getLogger("paramiko").setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    handlers=[RichHandler(show_path=False)]
+fmt = logging.Formatter(
+    "{asctime} {name} {levelname:10} {message}", style="{"
 )
+
+# file
+logfile = logging.FileHandler("logtt04.txt")
+logfile.setLevel(logging.INFO)
+logfile.setFormatter(fmt)
+
+logger.addHandler(logfile)
+
+# stderr
+
+stderr = RichHandler(show_path=False)
+stderr.setLevel(logging.INFO)
+logger.addHandler(stderr)
 
 
 def send_show_netmiko(device_dict, command):
     ip = device_dict["host"]
-    logging.info(f"===> Connection: {ip}")
+    logger.info(f"===> Connection: {ip}")
 
     with Netmiko(**device_dict) as ssh:
         ssh.enable()
         output = ssh.send_command(command)
-        logging.debug(f"<=== Received: output from {ip}\n{output}")
+        logger.debug(f"<=== Received: output from {ip}\n{output}")
     return output
 
 
