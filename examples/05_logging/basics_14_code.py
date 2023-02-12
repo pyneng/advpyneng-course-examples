a267f3b55f2f8dd893c13c849567338bfb1a0f43
pyneng <pyneng.course@gmail.com>
2023-02-12 13:08:04 +0000

    UPD 05_logging

--- examples/05_logging/basics_14_code.py
+++ examples/05_logging/basics_14_code.py
@@ -2,17 +2,22 @@ import logging
 from rich import inspect
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.NullHandler())
+logger = logging.getLogger("test")
+#logger.setLevel(logging.DEBUG)
+#logger.addHandler(logging.NullHandler())
 
-# fmt = logging.Formatter("{asctime} {name} {levelname:10} {message}", style="{")
-#
-# stderr = logging.StreamHandler()
-# stderr.setLevel(logging.DEBUG)
-# stderr.setFormatter(fmt)
-#
-# logger.addHandler(stderr)
+fmt = logging.Formatter("{asctime} {name:10} {levelname:10} {message}", style="{")
+
+stderr = logging.StreamHandler()
+stderr.setLevel(logging.DEBUG)
+stderr.setFormatter(fmt)
+
+logger.addHandler(stderr)
+print(f"{logger.getEffectiveLevel()=}")
+
+
+new = logging.getLogger("test.new")
+new.warning("message warning")
 
 def func():
     logger.debug("message debug")
