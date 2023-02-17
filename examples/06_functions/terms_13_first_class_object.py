356625f159b4c7f9837718073925ffaef8d0e806
pyneng <pyneng.course@gmail.com>
2023-02-17 17:56:21 +0000

    UPD 06_functions

--- examples/06_functions/terms_13_first_class_object.py
+++ examples/06_functions/terms_13_first_class_object.py
@@ -1,16 +1,17 @@
 """
 В Python все функции являются объектами первого класса.
 Python поддерживает:
-* создание функций в runtime
 * передачу функций в качестве аргументов другим функциям
 * возвращение функции как результата других функций
-* присваивание функций переменным, сохранение функций в структурах данных
+* присваивание функций переменным
+* сохранение функций в структурах данных
 """
+import time
 from getpass import getpass
 from rich import inspect
 
 
-def check_passwd(username, password, min_length=8, check_username=True):
+def check_passwd(username, password, *, min_length=8, check_username=True):
     if len(password) < min_length:
         return False
     elif check_username and username in password:
@@ -18,18 +19,24 @@ def check_passwd(username, password, min_length=8, check_username=True):
     else:
         return True
 
+# check_passwd.test = True
+# inspect(check_passwd, dunder=True)
+# inspect(check_passwd.__code__, dunder=True)
 
-def select_request_function(param):
-    choice = {
+def delay(seconds, func, *args, **kwargs):
+    print(f"{args=} {kwargs=}")
+    print(f"Delay {seconds} ...")
+    time.sleep(seconds)
+    return func(*args, **kwargs)
+
+
+def select_prompt_func(param):
+    func_choice = {
         "username": input,
         "password": getpass,
         "secret": getpass,
     }
-    return choice.get(param, input)
-
-check_passwd.test = True
-inspect(check_passwd, dunder=True)
-inspect(check_passwd.__code__, dunder=True)
+    return func_choice.get(param, input)
 
 
 class CiscoSSH:
@@ -40,7 +47,6 @@ class CiscoSSH:
             "secret": getpass,
         }
         for param in params:
-            if not param in device_params:
+            if param not in device_params:
                 function = params[param]
                 device_params[param] = function(f"{param.capitalize()}: ")
-
