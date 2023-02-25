d727a154cd582b8b0312e2b7af3e66619715410d
pyneng <pyneng.course@gmail.com>
2023-02-25 10:21:37 +0000

    UPD decorator

--- examples/08_decorators/02_decorator_with_args/examples_01_register_with_arg.py
+++ examples/08_decorators/02_decorator_with_args/examples_01_register_with_arg.py
@@ -1,13 +1,28 @@
 url_func_dict = {}
 
 
+def route(link):
+    print(f"route {link}")
+    def decorator(func):
+        print(f"decorator {func}")
+        url_func_dict[link] = func
+        return func
+
+    return decorator
 
 
 @route("/")
 def index():
     pass
 
+
+# decorator = route("/")
+# index = decorator(index)
+
+
 @route("/pyneng")
 def pyneng_func():
     pass
 
+
+print(url_func_dict)
--- examples/08_decorators/02_decorator_with_args/examples_03_restrict_type.py
+++ examples/08_decorators/02_decorator_with_args/examples_03_restrict_type.py
@@ -1,24 +1,30 @@
 from functools import wraps
 
 
-def all_str(func):
-    @wraps(func)
-    def wrapper(*args):
-        if not all(isinstance(arg, str) for arg in args):
-            raise ValueError("Все аргументы должны быть строками")
-        return func(*args)
-
-    return wrapper
+# def all_str_old(func):
+#    @wraps(func)
+#    def inner(*args):
+#        if not all(isinstance(arg, str) for arg in args):
+#            raise ValueError("Все аргументы должны быть строками")
+#        return func(*args)
+#
+#    return inner
+#
+#
+# @all_str_old
+# def upper(string):
+#    return string.upper()
 
 
 def arg_type(required_type):
-    print(f"Создаем декоратор с типом {required_type}")
+    print(f"create decorator for type {required_type}")
 
     def decorator(func):
-        print("Вызываю декоратор")
+        print(f"decorate {func}")
 
         @wraps(func)
         def inner(*args):
+            print("inner")
             if not all(isinstance(arg, required_type) for arg in args):
                 raise ValueError(f"Все аргументы должны быть {required_type.__name__}")
             return func(*args)
@@ -31,28 +37,23 @@ def arg_type(required_type):
 all_str = arg_type(str)
 all_int = arg_type(int)
 
-# f = all_str(f)
+
+@all_str
+def upper(string):
+    return string.upper()
 
 
-def arg_type2(required_type, func):
-    print(f"Создаем декоратор с типом {required_type}")
-    print("Вызываю декоратор")
-
-    @wraps(func)
-    def inner(*args):
-        if not all(isinstance(arg, required_type) for arg in args):
-            raise ValueError(f"Все аргументы должны быть {required_type.__name__}")
-        return func(*args)
-
-    return inner
+# decorator = arg_type(str)
+# upper = decorator(upper)
 
 
-def join(*args):
-    return ",".join(args)
+@all_str
+def lower(string):
+    print(f"lower {string=}")
+    return string.lower()
 
 
-def f():
-    pass
-
-
-join = arg_type2(str, join)
+@all_str
+def capitalize(string):
+    print(f"capitalize {string=}")
+    return string.capitalize()
