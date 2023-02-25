c3f1525cdaee6abb42d2b8cdbc31fb5023772b65
pyneng <pyneng.course@gmail.com>
2023-02-25 13:50:02 +0000

    UPD decorator

--- examples/08_decorators/03_decorating_classes/examples_03_decorating_class_methods.py
+++ examples/08_decorators/03_decorating_classes/examples_03_decorating_class_methods.py
@@ -1,79 +1,26 @@
-import paramiko
-import time
-from ex23_decorating_classes_add_new_method import add_pprint
+from functools import wraps
 
 
 def verbose(func):
+    print(f"verbose {func=}")
+
+    @wraps(func)
     def inner(*args, **kwargs):
-        print(f"Вызываю функцию {func.__name__}")
-        print("Аргументы", args[1:], kwargs)
-        return func(*args, **kwargs)
+        print(f"{func.__name__} {args[1:]=} {kwargs=}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} {result=}")
+        return result
 
     return inner
 
 
-def verbose_methods(cls):
-    methods = {
-        name: method
-        for name, method in vars(cls).items()
-        if not name.startswith("__") and callable(method)
-    }
-    for name, method in methods.items():
-        # name "send_show_command"
-        # method = verbose(method)
-        setattr(cls, name, verbose(method))
-    return cls
-
-
-@add_pprint
-@verbose_methods
-class BaseSSH:
-    device_type = "cisco_ios"
-
-    def __init__(self, ip, username, password):
+class IPAddress:
+    def __init__(self, ip, mask):
         self.ip = ip
-        self.username = username
-        self.password = password
-        self._MAX_READ = 10000
-
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        client.connect(
-            hostname=ip,
-            username=username,
-            password=password,
-            look_for_keys=False,
-            allow_agent=False,
-        )
-
-        self._ssh = client.invoke_shell()
-        time.sleep(1)
-        self._ssh.recv(self._MAX_READ)
-
-    def send_show_command(self, command):
-        self._ssh.send(command + "\n")
-        time.sleep(2)
-        result = self._ssh.recv(self._MAX_READ).decode("ascii")
-        return result
-
-    def send_config_commands(self, commands):
-        if isinstance(commands, str):
-            commands = [commands]
-        for command in commands:
-            self._ssh.send(command + "\n")
-            time.sleep(0.5)
-        result = self._ssh.recv(self._MAX_READ).decode("ascii")
-        return result
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self._ssh.close()
-
-    def close(self):
-        self._ssh.close()
+        self.mask = mask
 
     def __repr__(self):
-        return f"BaseSSH(ip={self.ip})"
+        return f"IPAddress({self.ip}/{self.mask})"
+
+    def bin_mask(self):
+        return "1" * self.mask + "0" * (32 - self.mask)
---
+++ examples/08_decorators/03_decorating_classes/examples_04_decorating_class_methods_all.py
@@ -0,0 +1,80 @@
+from functools import wraps
+import time
+import paramiko
+
+
+def verbose(func):
+    print(f"verbose {func=}")
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print(f"{func.__name__} {args[1:]=} {kwargs=}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} {result=}")
+        return result
+
+    return inner
+
+
+def verbose_methods(cls):
+    methods = {
+        name: method
+        for name, method in vars(cls).items()
+        if not name.startswith("__") and callable(method)
+    }
+    for name, method in methods.items():
+        setattr(cls, name, verbose(method))
+    return cls
+
+
+@verbose_methods
+class BaseSSH:
+    device_type = "cisco_ios"
+
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self._MAX_READ = 10000
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        client.connect(
+            hostname=ip,
+            username=username,
+            password=password,
+            look_for_keys=False,
+            allow_agent=False,
+        )
+
+        self._ssh = client.invoke_shell()
+        time.sleep(1)
+        self._ssh.recv(self._MAX_READ)
+
+    def send_show_command(self, command):
+        self._ssh.send(command + "\n")
+        time.sleep(2)
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
+        return result
+
+    def send_config_commands(self, commands):
+        if isinstance(commands, str):
+            commands = [commands]
+        for command in commands:
+            self._ssh.send(command + "\n")
+            time.sleep(0.5)
+        result = self._ssh.recv(self._MAX_READ).decode("ascii")
+        return result
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._ssh.close()
+
+    def close(self):
+        self._ssh.close()
+
+    def __repr__(self):
+        return f"BaseSSH(ip={self.ip})"
