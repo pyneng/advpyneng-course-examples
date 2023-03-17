94bf3153542e99bad9eee285de384ce08039045d
pyneng <pyneng.course@gmail.com>
2023-03-17 18:17:45 +0000

    UPD adv

--- examples/10_oop_special_methods/basics_04_timer_context_manager.py
+++ examples/10_oop_special_methods/basics_04_timer_context_manager.py
@@ -1,26 +1,33 @@
 import time
+from datetime import datetime
 
 
 class RunningTime:
+    def __init__(self):
+        print("__init__")
+
     def __enter__(self):
         print("__enter__")
-        self.start = time.time()
+        self.start = datetime.now()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        print("__exit__", exc_type, exc_value, traceback)
-        self.stop = time.time()
+        print(f"__exit__ {exc_type=} {exc_value=} {traceback=}")
+        self.stop = datetime.now()
         print(f"Running time: {self.stop - self.start}")
 
 
-t = RunningTime()
-
-with t:
+with RunningTime():
     time.sleep(2)
 
-with RunningTime() as rtime:
+with RunningTime() as run:
+    print("-" * 10)
     time.sleep(2)
 
-print(rtime.start)
-print(rtime.stop)
+
+run = RunningTime()
+with run:
+    print("-" * 10)
+    time.sleep(2)
+
 
