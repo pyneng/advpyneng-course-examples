0f898975b0a7ba4746a4344be9aca7f7e75343ca
pyneng <pyneng.course@gmail.com>
2023-03-31 17:56:10 +0000

    UPD advpyneng

---
+++ examples/12_oop_inheritance/mixin_01_inspect.py
@@ -0,0 +1,21 @@
+from rich import inspect
+
+class InspectMixin:
+    def _inspect(self, cls=False, **kwargs):
+        """kwargs rich.inspect args"""
+        if cls:
+            inspect(type(self), **kwargs)
+        else:
+            inspect(self, **kwargs)
+
+
+class IPAddress(InspectMixin):
+    def __init__(self, ip, mask):
+        self.ip = ip
+        self.mask = mask
+
+    def __repr__(self):
+        return f"IPAddress({self.ip}/{self.mask})"
+
+    def bin_mask(self):
+        return "1" * self.mask + "0" * (32 - self.mask)
