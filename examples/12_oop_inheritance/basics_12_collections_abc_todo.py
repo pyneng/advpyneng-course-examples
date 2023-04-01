dd56563d39b7d10063d8f0c3b6f1acbd0781ec98
pyneng <pyneng.course@gmail.com>
2023-04-01 19:01:28 +0000

    UPD 12_oop_inheritance

--- examples/12_oop_inheritance/basics_12_collections_abc_todo.py
+++ examples/12_oop_inheritance/basics_12_collections_abc_todo.py
@@ -1,39 +1,5 @@
-from functools import total_ordering
-from datetime import datetime
 from tabulate import tabulate
-
-
-@total_ordering
-class Task:
-    def __init__(self, title, description=None, due=None):
-        self.title = title
-        self.description = description
-        if due is None:
-            self.due = datetime.now().date()
-        else:
-            self.due = datetime.strptime(due, "%d/%m/%Y").date()
-
-    def __str__(self):
-        return f"{self.__class__.__name__}({'self.title'})"
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__} title='{self.title}' due={self.due}>"
-
-    def __lt__(self, other):  # <
-        print(f"Task __lt__ {self=} {other=}")
-        if not isinstance(other, Task):
-            return NotImplemented
-        return self.due < other.due
-
-    def __eq__(self, other):  # ==
-        print(f"Task __eq__ {self=} {other=}")
-        if not isinstance(other, Task):
-            return NotImplemented
-        return (self.title, self.description, self.due) == (
-            other.title,
-            other.description,
-            other.due,
-        )
+from class_task import Task
 
 
 class TodoList:
@@ -49,8 +15,7 @@ class TodoList:
     def __str__(self):
         if not self._tasks:
             return f"{repr(self)}. You're all done!"
-        # table = [vars(task) for task in self._tasks]
-        table = [task.to_dict() for task in self._tasks]
+        table = [vars(task) for task in self._tasks]
         return tabulate(table, headers="keys")
 
     def __getitem__(self, index):
---
+++ examples/12_oop_inheritance/class_task.py
@@ -0,0 +1,36 @@
+from functools import total_ordering
+from datetime import datetime
+from tabulate import tabulate
+
+
+@total_ordering
+class Task:
+    def __init__(self, title, description=None, due=None):
+        self.title = title
+        self.description = description
+        if due is None:
+            self.due = datetime.now().date()
+        else:
+            self.due = datetime.strptime(due, "%d/%m/%Y").date()
+
+    def __lt__(self, other):  # <
+        print(f"Task __lt__ {self=} {other=}")
+        if not isinstance(other, Task):
+            return NotImplemented
+        return self.due < other.due
+
+    def __eq__(self, other):  # ==
+        print(f"Task __eq__ {self=} {other=}")
+        if not isinstance(other, Task):
+            return NotImplemented
+        return (self.title, self.description, self.due) == (
+            other.title,
+            other.description,
+            other.due,
+        )
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({'self.title'})"
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} title='{self.title}' due={self.due}>"
