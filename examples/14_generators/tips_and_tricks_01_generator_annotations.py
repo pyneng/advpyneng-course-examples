9fd77aac563780a8973b39f54cd103c36ca0170a
pyneng <pyneng.course@gmail.com>
2023-04-15 16:57:25 +0000

    14_generators

--- examples/14_generators/tips_and_tricks_01_generator_annotations.py
+++ examples/14_generators/tips_and_tricks_01_generator_annotations.py
@@ -1,14 +1,17 @@
 from pprint import pprint
+from collections.abc import Iterator, Generator
+# Generator[YieldType, SendType, ReturnType]
 
 
-def my_range(start, stop):
+# def my_range(start: int, stop: int) -> Generator[int, None, None]:
+def my_range(start: int, stop: int) -> Iterator[int]:
     current = start
     while current < stop:
         yield current
         current += 1
 
 
-def filter_cfg(filename):
+def filter_cfg(filename: str) -> Iterator[str]:
     with open(filename) as f:
         for line in f:
             if line.startswith("interface"):
--- examples/14_generators/tips_and_tricks_02_generator_pipeline_annotations.py
+++ examples/14_generators/tips_and_tricks_02_generator_pipeline_annotations.py
@@ -1,15 +1,16 @@
 import re
+from collections.abc import Iterator, Iterable
 from rich import print as rprint
 
 
-def is_ignore_line(line, ignore_lines):
+def is_ignore_line(line: str, ignore_lines: Iterable[str]) -> bool:
     for ignore in ignore_lines:
         if ignore in line:
             return True
     return False
 
 
-def clean_config(config_filename, ignore_lines_with):
+def clean_config(config_filename: str, ignore_lines_with: Iterable[str]) -> Iterator[str]:
     with open(config_filename) as f:
         for index, line in enumerate(f, 1):
             print("READ", index, line.rstrip())
@@ -17,14 +18,14 @@ def clean_config(config_filename, ignore_lines_with):
                 yield line.rstrip()
 
 
-def filter_lines(iterable, regex):
+def filter_lines(iterable: Iterable[str], regex: str) -> Iterator[str]:
     for line in iterable:
         if re.search(regex, line):
             rprint(f"[green]FILTER {line}")
             yield line
 
 
-def convert_to_lower(iterable):
+def convert_to_lower(iterable: Iterable[str]) -> Iterator[str]:
     for line in iterable:
         rprint(f"[violet]LOWER {line}")
         yield line.lower()
