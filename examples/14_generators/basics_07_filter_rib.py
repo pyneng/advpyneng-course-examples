e4fed6986febaf988e290926a1e39f38a1adc506
pyneng <pyneng.course@gmail.com>
2023-04-15 19:10:32 +0000

    14_generators

--- examples/14_generators/basics_07_filter_rib.py
+++ examples/14_generators/basics_07_filter_rib.py
@@ -1,4 +1,5 @@
 import csv
+from rich import print as rprint
 
 # "status","network","netmask","nexthop","metric","locprf","weight","path","origin"
 # "*","1.0.0.0","24","200.219.145.45",NA,NA,0,"28135 18881 3549 15169","i"
@@ -6,35 +7,32 @@ import csv
 # "*","1.0.4.0","24","200.219.145.45",NA,NA,0,"28135 18881 3549 1299 7545 56203","i"
 # "*>","1.0.4.0","24","200.219.145.23",NA,NA,0,"53242 12956 174 7545 56203","i"
 
-def read_file(filename):
+def read_rib(filename):
     with open(filename) as f:
         reader = csv.reader(f)
         header = next(reader)
-        for index, l_list in enumerate(reader, 1):
-            #print("READ", index, l_list)
-            yield index, l_list
+        for index, row in enumerate(reader, 1):
+            print("READ", index, row)
+            yield row
 
 
 def filter_by_nhop(iterable, nhop):
-    for index, prefix in iterable:
+    for prefix in iterable:
         if prefix[3] == nhop:
-            yield index, prefix
+            yield prefix
 
 
 def filter_by_mask(iterable, mask):
-    for index, prefix in iterable:
+    for prefix in iterable:
         if prefix[2] == mask:
-            yield index, prefix
+            yield prefix
 
 
 if __name__ == "__main__":
-    file = read_file("rib.csv")
-    f_23 = filter_by_nhop(file, "200.219.145.23")
-    mask_22 = filter_by_mask(f_23, "22")
-    print(next(mask_22))
-    print(next(mask_22))
-    print(next(mask_22))
-    print(next(mask_22))
-    print(next(mask_22))
-    print(next(mask_22))
+    f = read_rib("rib_table.csv")
+    filter_prefix = filter_by_nhop(f, "200.219.145.23")
+    f_mask = filter_by_mask(filter_prefix, "22")
 
+    for _ in range(4):
+        line = next(f_mask)
+        rprint(f"[violet]>>> {line}")
