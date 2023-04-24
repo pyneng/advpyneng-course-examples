7fe64a08e5987dd4c9777ffda77f1cb49f4e3ff4
pyneng <pyneng.course@gmail.com>
2023-04-24 16:20:37 +0000

    17_async_libraries

--- examples/17_async_libraries/scrapli_01_basics.py
+++ examples/17_async_libraries/scrapli_01_basics.py
@@ -3,29 +3,25 @@ import yaml
 from pprint import pprint
 from scrapli import AsyncScrapli
 from scrapli.exceptions import ScrapliException
-from devices_scrapli import devices
+
 
 async def send_show(device, command):
     try:
-        async with AsyncScrapli(**device) as conn:
-            reply = await conn.send_command(command)
-            parsed = reply.textfsm_parse_output()
-        return parsed
+        async with AsyncScrapli(**device) as ssh:
+            response = await ssh.send_command(command)
+            return response.result
     except ScrapliException as error:
         print(error)
 
 
-
 async def run_all(devices, command):
-    coro = [send_show(dev, command) for dev in devices]
-    result = await asyncio.gather(*coro, return_exceptions=True)
-    return  result
+    coroutines = [send_show(dev, command) for dev in devices]
+    results = await asyncio.gather(*coroutines, return_exceptions=True)
+    return results
 
 
 if __name__ == "__main__":
     with open("devices_scrapli_alg.yaml") as f:
         devices = yaml.safe_load(f)
-    pprint(devices)
     output = asyncio.run(run_all(devices, "show ip int br"))
-    pprint(output)
-
+    print(output)
