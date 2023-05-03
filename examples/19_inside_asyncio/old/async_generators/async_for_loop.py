7962911df19dda8cde77016a1833d9cf8d52aa24
pyneng <pyneng.course@gmail.com>
2023-05-03 17:43:23 +0000

    UPD asyncio

--- examples/19_inside_asyncio/old/async_generators/async_for_loop.py
+++
@@ -1,8 +0,0 @@
-async def async_for(items):
-    iterator = items.__aiter__()
-    while True:
-        try:
-            item = await iterator.__anext__()
-            print(item)
-        except StopAsyncIteration:
-            break
