37469b3a62f88d5cc29876b2b38f07a0260ea892
pyneng <pyneng.course@gmail.com>
2023-05-04 16:22:51 +0000

    UPD 19_inside_asyncio

--- examples/19_inside_asyncio/extra_02_task_factory.py
+++ examples/19_inside_asyncio/extra_02_task_factory.py
@@ -1,83 +1,73 @@
 import asyncio
+import re
 from datetime import datetime
 from asyncio.tasks import _PyTask
 from functools import wraps
-from click import secho
+from rich import print as rprint
 
 
 def verbose(func):
     @wraps(func)
     def inner(*args, **kwargs):
         self = args[0]
-        print(f">>> VERBOSE {func.__qualname__:30} args={args[1:]}")
+        rprint(f"[violet]Calling {func.__qualname__}\n{self}[/violet]")
+        # print(f"Calling {func.__qualname__:30} args={args[1:]}")
         result = func(*args, **kwargs)
-        print(f"<<< VERBOSE {func.__qualname__:30} {result=}")
+        # print(f"Calling {func.__qualname__:30} {result=}")
         return result
 
     return inner
 
 
-def verbose_methods(cls):
-    ignore = ("_repr_info", "get_name")
-    methods = {}
-    for name in dir(cls):
-        method = getattr(cls, name)
-        if (
-            not name.startswith("__")
-            and callable(method)
-            and name not in ignore
-            and "Future" not in method.__qualname__
-        ):
-            methods[name] = method
-    for name, method in methods.items():
-        setattr(cls, name, verbose(method))
-    return cls
+def verbose_methods_filter(include=None, ignore=None):
+    if ignore is None:
+        ignore = set()
+    if include is None:
+        include = set()
+
+    def verbose_methods(cls):
+        all_attrs = set(dir(cls))
+        if include:
+            all_attrs = include
+        elif ignore:
+            all_attrs -= ignore
+        methods = {}
+        for name in all_attrs:
+            method = getattr(cls, name)
+            if callable(method) and not re.search(r"__\S+__", name):
+                methods[name] = method
+        for name, method in methods.items():
+            setattr(cls, name, verbose(method))
+        return cls
+
+    return verbose_methods
+
+
+# verbose_methods = verbose_methods_filter()
+verbose_methods = verbose_methods_filter(include=["_Task__step", "_Task__wakeup"])
 
 
 async def delay_msg(delay, msg):
-    print(f"START delay_msg {msg=} {time()}")
+    print(f"START delay_msg {msg=}")
     await asyncio.sleep(delay)
-    print(f"STOP  delay_msg {msg=} {time()}")
+    print(f"STOP  delay_msg {msg=}")
     return msg
 
 
 async def main():
     print(f">>> START   MAIN")
-    # res1 = await delay_msg(3, "task1")
-    # res1 = await asyncio.create_task(delay_msg(3, "task1"))
-    await asyncio.sleep(5)
-    #print(f">>> MIDDLE  MAIN")
-    #await asyncio.sleep(5)
-    print(f">>> STOP    MAIN")
-
-
-@verbose_methods
-class CustomTask(_PyTask):
-    def __init__(self, coro, *, loop=None, name=None):
-        super().__init__(coro, loop=loop, name=name)
-        self._total_step = 0
-
-    def _Task__step(self, exc=None):
-        self._total_step += 1
-        secho(
-            f"==> STEP{self._total_step} {self.get_name().upper()} {self}", fg="green"
-        )
-        super()._Task__step(exc=exc)
-        secho(
-            f"<== STEP{self._total_step} {self.get_name().upper()} {self}", fg="green"
-        )
-
-    def __repr__(self):
-        orig_repr = super().__repr__()
-        return f"CustomTask {orig_repr}"
+    # res = await delay_msg(3, "task1")
+    await asyncio.sleep(3)
+    print(f"<<< STOP    MAIN")
 
 
 def task_factory(loop, coro):
+    CustomTask = verbose_methods(_PyTask)
     return CustomTask(coro, loop=loop)
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     loop.set_task_factory(task_factory)
     task1 = loop.run_until_complete(main())
     loop.close()
